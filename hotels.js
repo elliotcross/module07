5c01aab2ce379a56f2258c7394546300635a0d85
elliotcross <114322766+elliotcross@users.noreply.github.com>
2022-11-04 16:07:08 -0700

    Update hotels.js

--- hotels.js
+++ hotels.js
@@ -9,9 +9,10 @@ function Hotel(name, rooms, booked) {
 }
 
 
-// Create two hotel objects
+// Create three hotel objects
 var quayHotel = new Hotel('Quay', 40, 25);
 var parkHotel = new Hotel('Park', 120, 77);
+var sunsetHotel = new Hotel('Sunset', 86, 10);
 
 
 // Update the HTML for the page
@@ -25,7 +26,12 @@ var details2 = parkHotel.name + ' rooms: ';
 var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
 
+var details3 = sunsetHotel.name + ' rooms: ';
+    details3 += sunsetHotel.checkAvailability();
+var elHotel3 = document.getElementById('hotel3');
+elHotel3.textContent = details3;
+
 /* 
 NOTE: textContent does not work in IE8 or earlier
-You can use innerHTML on lines 21 and 26, but note the security issues on p228-231
+You can use innerHTML on lines 21, 26, and 31, but note the security issues on p228-231
 */
